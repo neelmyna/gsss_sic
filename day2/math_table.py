9fe6d39b2a2fa0ca28608773d9f262efa83e7bfb
neelmyna <nithincheeta@gmail.com>
2025-08-13 10:23:45 +0530

    day3 session1 notes and programs added

---
+++ day2/math_table.py
@@ -0,0 +1,9 @@
+import sys
+
+print(sys.argv) # printing the argv list
+print(type(sys.argv)) # printing type of argv
+print(sys.argv[0]) # printing 1st element in the list argv
+print(sys.argv[1]) # printing 2nd element in the list argv
+print(type(sys.argv[0]))
+print(type(sys.argv[1]))
+print(sys.argv[2]) # printing 3rd element in the list argv
